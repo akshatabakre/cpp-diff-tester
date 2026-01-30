21d49efc06598b88656eb0498af42316c94771de
akshatabakre <akshatabakre@gmail.com>
2026-01-30 19:25:10 +0530

    multiple testcases

--- generator.cpp
+++ generator.cpp
@@ -6,30 +6,39 @@ int main() {
     ifstream fin("constraints.txt");
 
     string var;
+    int T_min, T_max;
+    fin >> var >> T_min >> T_max;
+
+    int T = rand() % (T_max - T_min + 1) + T_min;
+    cout << T << endl;
+
+    string n_var;
     int n_min, n_max;
-    fin >> var >> n_min >> n_max;
-
-    int n;
-    int mode = rand() % 5;
-
-    if(mode == 0) n = n_min;
-    else if(mode == 1) n = n_max;
-    else n = rand() % (n_max - n_min + 1) + n_min;
+    fin >> n_var >> n_min >> n_max;
 
     string arr, size_var;
     int a_min, a_max;
     fin >> arr >> size_var >> a_min >> a_max;
 
-    cout << n << endl;
+    for(int tc = 0; tc < T; tc++) {
+        int n;
+        int mode = rand() % 5;
 
-    for(int i = 0; i < n; i++) {
-        int x;
-        if(mode == 2) x = 0;
-        else if(mode == 3) x = a_min;
-        else if(mode == 4) x = a_max;
-        else x = rand() % (a_max - a_min + 1) + a_min;
+        if(mode == 0) n = n_min;
+        else if(mode == 1) n = n_max;
+        else n = rand() % (n_max - n_min + 1) + n_min;
 
-        cout << x << " ";
+        cout << n << endl;
+
+        for(int i = 0; i < n; i++) {
+            int x;
+            if(mode == 2) x = 0;
+            else if(mode == 3) x = a_min;
+            else if(mode == 4) x = a_max;
+            else x = rand() % (a_max - a_min + 1) + a_min;
+
+            cout << x << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
 }
--- optimal.cpp
+++ optimal.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int>a(n);
-    for(int i=0;i<n;i++) cin >> a[i];
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++) cin >> a[i];
 
-    int cur = 0, best = INT_MIN;
-    for(int x : a){
-        cur += x;
-        best = max(best, cur);
-        // if(cur < 0) cur = 0;
+        int cur = 0, best = INT_MIN;
+        for(int x : a){
+            cur += x;
+            best = max(best, cur);
+            if(cur < 0) cur = 0;
+        }
+        cout << best << endl;
     }
-    cout << best << endl;
 }
